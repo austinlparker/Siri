2f6e1764a85c1008c3549719e403aa282eb415ab
Alan <alanlove@email.arizona.edu>
2013-11-05 10:11:51 -0700

    aaaa

--- plugins/web.py
+++ plugins/web.py
@@ -9,30 +9,38 @@ from util import hook
 @hook.api_key('web')
 @hook.regex('.*')
 def chat_log(match, nick=None, chan=None, bot=None, conn=None, api_key=None):
-    r = redis.Redis(host=api_key['redis']['address'],
-                    port=api_key['redis']['port'],
-                    db=api_key['redis']['db'])
-    message = {
-        "bot": conn.nick,
-        "user": nick,
-        "channel": chan.replace("#", ""),
-        "content": match.group(),
-        "server": conn.server,
-        "timestamp": str(int(time.time()))
-    }
-    r.publish('chat', json.dumps(message))
+    try:
+
+        r = redis.Redis(host=api_key['redis']['address'],
+                        port=api_key['redis']['port'],
+                        db=api_key['redis']['db'])
+        message = {
+            "bot": conn.nick,
+            "user": nick,
+            "channel": chan.replace("#", ""),
+            "content": match.group(),
+            "server": conn.server,
+            "timestamp": str(int(time.time()))
+        }
+        r.publish('chat', json.dumps(message))
+
+    except:
+        pass
 
 
 @hook.api_key('web')
 @hook.regex('.*')
 def update_channels(match, conn=None, api_key=None, chan=None):
-    r = redis.Redis(host=api_key['redis']['address'],
-                    port=api_key['redis']['port'],
-                    db=api_key['redis']['db'])
+    try:
+        r = redis.Redis(host=api_key['redis']['address'],
+                        port=api_key['redis']['port'],
+                        db=api_key['redis']['db'])
 
-    chan = chan.replace("#", "")
-    if not r.exists('irc_channels'):
-        r.sadd('irc_channels', chan)
-        r.expire('irc_channels', 300)
-    else:
-        r.sadd('irc_channels', chan)
+        chan = chan.replace("#", "")
+        if not r.exists('irc_channels'):
+            r.sadd('irc_channels', chan)
+            r.expire('irc_channels', 300)
+        else:
+            r.sadd('irc_channels', chan)
+    except:
+        pass
