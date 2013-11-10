deb87255e17d480c6069a8aa9a80e355d63e322d
Alan <alanlove@email.arizona.edu>
2013-11-09 19:58:40 -0700

    fixed some rethinkdb issues

--- web/app.py
+++ web/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, make_response
+from flask import Flask, render_template, request, make_response, g, abort
 from flask_sockets import Sockets
 from werkzeug.contrib.atom import AtomFeed
 
@@ -17,7 +17,6 @@ app.config.from_object('config')
 
 logger = IRCLogParser()
 
-rethinkdb.connect('localhost', 28015).repl()
 log_db = rethinkdb.db('siri').table('logs')
 url_db = rethinkdb.db('siri').table('urls')
 red = redis.Redis()
@@ -29,16 +28,32 @@ urlsocket = WebsocketBackend('irc_urls')
 urlsocket.start()
 
 
+@app.before_request
+def before_request():
+    try:
+        g.rdb_conn = rethinkdb.connect('localhost', 28015)
+    except:
+        abort(503, "No database connection could be established.")
+
+
+@app.teardown_request
+def teardown_request():
+    try:
+        g.rdb_conn.close()
+    except AttributeError:
+        pass
+
+
 @app.route('/')
 def index():
     active_channels = list(red.smembers('irc_channels'))
-    all_channels = [chan['channel'] for chan in log_db.pluck('channel').distinct().run()]
+    all_channels = [chan['channel'] for chan in log_db.pluck('channel').distinct().run(g.rdb_conn)]
     return render_template("index.html", active=active_channels, all_chans=all_channels)
 
 
 @app.route('/grid/<channel>')
 def channel_images(channel):
-    urls = list(url_db.filter({'channel': channel}).run())[:50]
+    urls = list(url_db.filter({'channel': channel}).run(g.rdb_conn))[:50]
     return render_template("grid.html", channel=channel, urls=urls)
 
 
@@ -49,7 +64,7 @@ def channel_urls(channel):
 
 @app.route('/rss/<channel>.atom')
 def rss(channel):
-    urls = list(url_db.filter({'channel': channel}).run())[:50]
+    urls = list(url_db.filter({'channel': channel}).run(g.rdb_conn))[:50]
     feed = AtomFeed('Recent URLs', feed_url=request.url, url=request.url_root)
     for url in urls:
         feed.add(url['title'], "This URL is a {0}".format(url['type']),
