87d67e06ac26ca4cf87eed8a4d6313cb706aeb5c
Alan <alanlove@email.arizona.edu>
2013-11-09 20:41:55 -0700

    added global messaging support

--- plugins/message.py
+++ plugins/message.py
@@ -7,7 +7,7 @@ log_db = rethinkdb.db('siri').table('logs')
 all_channels = [chan['channel'] for chan in log_db.pluck('channel').distinct().run(rdb_conn)]
 
 @hook.command
-def message_all(inp, chan=None, conn=None, nick=None):
+def shoutout(inp, chan=None, conn=None, nick=None):
     chans = ["#" + channel for channel in all_channels]
     admins = ['elgruntox']
     
