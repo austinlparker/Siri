bb8b53a8ec18c6f69a18a90fc42d57ea2cdde0b3
Nick Johnson <njohnson@limcollective.com>
2013-12-23 15:17:27 -0600

    adding doge/btc rate to doge command

--- plugins/doge.py
+++ plugins/doge.py
@@ -7,17 +7,26 @@ import lxml.html
 @hook.command
 def doge(inp, say=None):
     amount = inp
-    return "{1} doge is currently worth ${0}.".format(get_usd(amount), amount)
+    usd_amount,exchange_rate = get_usd(amount)
+    return "{0} doge is currently worth ${1}. (1 doge = {2} BTC)".format(amount, usd_amount, exchange_rate)
 
 
 def get_usd(amount=1000):
     if type(amount) != int:
         amount = 1000
 
+    r_btc = requests.get('http://dogepay.com/frame_converter.php?v=1&from_type=DOGE&to_type=BTC'.format(amount)).text
     r = requests.get('http://dogepay.com/frame_converter.php?v={0}&from_type=DOGE&to_type=USD'.format(amount)).text
+    
+    html_btc = lxml.html.fromstring(r_btc)
     html = lxml.html.fromstring(r)
-    amount = html.xpath('//font/text()')[0]
-    return amount.split('= ')[1].replace('$', '')
+    
+    btc_exchange_raw = html_btc.xpath('//font/text()')[0]
+    amount_raw = html.xpath('//font/text()')[0]
+    
+    btc_exchange_amt = btc_exchange_raw.split('= ')[1].replace('BTC ', '')
+    amount = amount_raw.split('= ')[1].replace('$', '')
+    return amount, btc_exchange_amt
 
 
 if __name__ == "__main__":
