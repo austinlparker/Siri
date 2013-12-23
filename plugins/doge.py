ca1a55043a53b64faeef175d010ff434079b473b
Alan <alanlove@email.arizona.edu>
2013-12-23 14:11:44 -0700

    DOGE

--- plugins/doge.py
+++ plugins/doge.py
@@ -6,21 +6,14 @@ import lxml.html
 
 @hook.command
 def doge(inp, say=None):
-    if inp:
-        try:
-            amount = int(inp)
-        except:
-            amount = 1000
-    else:
-        amount = 1000
-        
+    amount = inp
     return "{1} doge is currently worth ${0}.".format(get_usd(amount), amount)
 
 
 def get_usd(amount=1000):
     if type(amount) != int:
         amount = 1000
-        
+
     r = requests.get('http://dogepay.com/frame_converter.php?v={0}&from_type=DOGE&to_type=USD'.format(amount)).text
     html = lxml.html.fromstring(r)
     amount = html.xpath('//font/text()')[0]
