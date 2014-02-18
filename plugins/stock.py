dc4a509d5cd21fc4a1bb7892cb1a60e890c0b8f0
serewit <root@serewit.com>
2014-02-18 18:25:34 -0500

    added multiple quotes

--- plugins/stock.py
+++ plugins/stock.py
@@ -7,29 +7,36 @@ from util import hook, http
 def stock(inp):
     '''.stock <symbol> -- gets stock information'''
 
+    symbols = inp.split()
+
     url = ('http://query.yahooapis.com/v1/public/yql?format=json&'
            'env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
+    
+    ret_string = ''
 
-    parsed = http.get_json(url, q='select * from yahoo.finance.quote '
-                           'where symbol in ("%s")' % inp)  # heh, SQLI
+    for ticker in symbols:
+      parsed = http.get_json(url, q='select * from yahoo.finance.quote '
+                             'where symbol in ("%s")' % ticker)  # heh, SQLI
 
-    quote = parsed['query']['results']['quote']
+      quote = parsed['query']['results']['quote']
 
-    # if we dont get a company name back, the symbol doesn't match a company
-    if quote['Change'] is None:
-        return "unknown ticker symbol %s" % inp
+      # if we dont get a company name back, the symbol doesn't match a company
+      if quote['Change'] is None:
+          return "unknown ticker symbol %s" % ticker
 
-    if quote['Change'][0] == '-':
-        quote['color'] = "5"
-    else:
-        quote['color'] = "3"
+      if quote['Change'][0] == '-':
+          quote['color'] = "5"
+      else:
+          quote['color'] = "3"
 
-    quote['Percent_Change'] = (100 * float(quote['Change']) /
-                               float(quote['LastTradePriceOnly']))
+      quote['Percent_Change'] = (100 * float(quote['Change']) /
+                                 float(quote['LastTradePriceOnly']))
 
-    ret = "%(Name)s - %(LastTradePriceOnly)s "                   \
-          "\x03%(color)s%(Change)s (%(Percent_Change).2f%%)\x03 "        \
-          "Day Range: %(DaysRange)s " \
-          "MCAP: %(MarketCapitalization)s" % quote
+      ret = "%(Name)s - %(LastTradePriceOnly)s "                   \
+            "\x03%(color)s%(Change)s (%(Percent_Change).2f%%)\x03 "        \
+            "Day Range: %(DaysRange)s " \
+            "MCAP: %(MarketCapitalization)s" % quote
 
-    return ret
+      ret_string = ret_string + " | " + ret
+
+    return ret_string
